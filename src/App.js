fd5260ee036a1a24830deed510e11c41905484d4
Shevchenko Pavel <shevchenkopavel17@gmail.com>
2020-04-21 16:56:21 +0300

    to-hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const useImage = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => {
+      setImage(img.src)
+      setLoading(false)
+    };
+    img.onerror = () => setError('error');
+    return () => {
+      setLoading(true);
+      setError('');
+      setImage(null);
+    }
+  }, [url]);
+  return { image, loading, error };
+}
 
 const RenderImage = ({
   imageUrl = 'https://customgrowthgroup.com/wp-content/uploads/2013/09/URL-image.jpg',
   errorContent = <>error</>,
   loaderContent = <>loading</>,
-  placeholder = 'image',
+  alt = 'image',
   className,
   ...rest
 }) => {
 
-  const REGULAR_IMG = 'https://customgrowthgroup.com/wp-content/uploads/2013/09/URL-image.jpg';
-  const HUGE_IMG = 'https://i.imgur.com/1RpGWXY.png';
-
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-
-  const img = new Image();
-  img.src = imageUrl;
-  img.onload = () => setLoading(false);
-  img.onerror = () => setError('error');
-
-  const content = (
-    <img
-      src={img.src}
-      className={className}
-      alt={placeholder}
-      {...rest}
-    />
-    );
+  const { image, loading, error } = useImage(imageUrl); 
 
   if (error) return errorContent;
   if (loading) return loaderContent;
 
-  return content;
-
-//   return error 
-//         ? errorContent
-//         : loading
-//           ? loaderContent
-//           : content;
+  return (
+          <img
+            src={image}
+            alt={alt}
+            {...rest}
+          />
+  );
 }
 
 export default RenderImage;
